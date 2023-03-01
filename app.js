4359dda5bcbc76a2ed4c724a26ab73d33616ea70
Evan Hesketh <evanhesketh@yahoo.ca>
2023-02-28 20:05:35 -0500

    complete part 2

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ $('form').on('submit', getGif);
 async function getGif(event) {
     event.preventDefault();
     const searchTermValue = $("#gif-search").val();
-    const newGif = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTermValue}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`)
-    console.log(newGif);
+    const newGif = await axios.get(`http://api.giphy.com/v1/gifs/search?q=${searchTermValue}&api_key=MhAodEJIJxQMxW9XqxKjyXfNYdLoOIym`);
+    console.log('newGif.data, ', newGif.data);
+    addGifToPage(newGif.data.data[0].images.original.url);
 }
+
+function addGifToPage(gifImgUrl) {
+    const gifImg = `<img src=${gifImgUrl}>`;
+    $('#Gif-container').append(gifImg);
+}
+
